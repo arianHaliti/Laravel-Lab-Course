19bf0dd93fb9e2002e1bd3ee351297365630aa76
Arian Haliti <arian93haliti@gmail.com>
2018-05-29 16:56:29 +0200

    Added stats to right Blade

--- resources/views/inc/question/right.blade.php
+++ resources/views/inc/question/right.blade.php
@@ -1,3 +1,21 @@
+<?php 
+    use App\Tag;
+    use App\Question;
+    use App\Answer;
+    use App\User;
+    $tags= Tag::join('tag_questions','tag_questions.tag_id','=','tags.tag_id')
+            ->orderBy('count','desc')
+            ->limit(3)
+            ->groupBy('tag_questions.tag_id')
+            ->get(['tags.tag_name', 
+                DB::raw('COUNT(tag_questions.tag_id) AS count')
+            ]);
+    $question = Question::where('question_active',0)->count();
+    $answers = Answer::where('answer_active',0)->count();
+    $soleved = Answer::join('correct_answers','correct_answers.answer_id','=','answers.answer_id')
+                ->count();
+    $users = User::all()->count();
+?>
 <div class="col-md-3 mt-2">
         <div class="row px-2 mt-4">
         <div class="col-md-12 px-2 py-2 mt-2 transform1">
@@ -8,18 +26,21 @@
             <div class="col-md-12 px-2 mt-3">
                 <h5 class="border-bottom border-top mb-0 p-2 transform1 text-muted">Overall stats</h5>
             <ul>
-                <li><a href="#">15000</a></li>
-                <li><a href="#">HTML</a></li>
-                <li><a href="#">CSSgit</a></li>
+                    <li> {{$question}} Questions asked</li>
+                    <li> {{$answers}} Answeres given </li>
+                    <li> {{$soleved}} Questions solved </li>
+                    <li> {{$users}} Active Users </li>
+               
             </ul>
             </div>
             
             <div class="col-md-12 px-2 mt-3">
                 <h5 class="border-bottom border-top mb-0 p-2 transform1 text-muted">Most used tags</h5>
             <ul>
-                <li><a href="#">html</a></li>
-                <li><a href="#">css</a></li>
-                <li><a href="#">java</a></li>
+                @foreach($tags as $t)
+                    <li><a href="#">{{$t->tag_name}} </a> <small>x {{$t->count}}</small></li>
+                @endforeach
+
             </ul>
             </div>
         </div>
