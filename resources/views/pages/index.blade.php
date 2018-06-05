be2e861d085a237f90e2475c304420d5c5b3b09f
yllzon <yllzon.sejdiu97@gmail.com>
2018-06-05 11:26:45 +0200

    Footer is done

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -123,8 +123,78 @@ use App\Question;
                 
             </div>
         </div>
+		
+            
             </div>
-           
+			
+			
+		   <div class="footer container-fluid border-top py-4 bg-light mt-4">
+				<div class="container">
+				   <div class="row p-0">
+					  
+						  <div class="col-md-1">
+							<img src="/storage/image/logo.png">
+						  </div>
+						    <div class="col-md-2">
+								<h5 class="transform1 mb-3 text-muted">LAB 2</h5>
+								<ul class="pl-0 list-style-none">
+									<li><a href="#" class="text-muted">Questions</a></li>
+									<li><a href="#" class="text-muted">Tutorials</a></li>
+									<li><a href="#" class="text-muted">Users</a></li>
+									<li><a href="#" class="text-muted">Tags</a></li>
+								</ul>
+						  </div>
+						  
+						  <div class="col-md-2">
+								<h5 class="transform1 mb-3 text-muted">Company</h5>
+								<ul class="pl-0 list-style-none ">
+									<li><a href="#" class="text-muted">About</a></li>
+									<li><a href="#" class="text-muted">Tutorials</a></li>
+									<li><a href="#" class="text-muted">Users</a></li>
+									<li><a href="#" class="text-muted">Tags</a></li>
+								</ul>
+						  </div>
+						  
+						  <div class="col-md-7 float-right">
+								
+								
+								<ul class="pl-0 list-style-none float-right">
+								<h5 class="transform1 mb-3 text-muted">Contact Us</h5>
+									<li class="text-muted">Nr.tel#1: +377(0)123123</li>
+
+									<li class="text-muted">Nr.tel#2: +377(0)334232</li>
+
+									<li class="text-muted">Email: labcourse2@gmail.com</li>
+									<li class="text-muted">Location: Kosovo,Prishtine</li>
+																		
+								</ul>
+						  </div>
+					 
+					 
+				  </div>
+				</div>
+			</div>
+			
+			
+			 <div class="footer2 container-fluid border-top bg-light py-4">
+				<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<p class="mb-0 f-14">&copy; 2018 LabCourse2 | All rights reserved</p>
+					</div>
+					
+					<div class="col-md-6">
+						<ul class="socials float-right mb-0">
+							<li class="mr-1"><a href="#" class="text-muted"><i class="fab fa-facebook-f"></i></a></li>
+							<li class="mr-1"><a href="#" class="text-muted"><i class="fab fa-twitter"></i></a></li>
+							<li class="mr-1"><a href="#" class="text-muted"><i class="fab fa-linkedin-in"></i></a></li>
+							<li><a href="#" class="text-muted"><i class="fab fa-youtube"></i></a></li>
+						</ul>
+					</div>
+					
+				</div>
+				</div>
+			 </div>
 
   
 @endsection
--- resources/views/pages/tag.blade.php
+++ resources/views/pages/tag.blade.php
@@ -81,28 +81,7 @@
                 
                 
             </div>
-            <div class="col-md-3 mt-2">
-                <div class="row px-2 mt-4">
-                
-                    <div class="col-md-12 px-2 mt-3">
-                        <h5 class="border-bottom border-top mb-0 p-2 transform1 text-muted">Overall stats</h5>
-                      <ul>
-                        <li><a href="#">100000</a></li>
-                        <li><a href="#">css</a></li>
-                        <li><a href="#">java</a></li>
-                      </ul>
-                    </div>
-                    
-                    <div class="col-md-12 px-2 mt-3">
-                        <h5 class="border-bottom border-top mb-0 p-2 transform1 text-muted">Most used tags</h5>
-                      <ul>
-                        <li><a href="#">html</a></li>
-                        <li><a href="#">css</a></li>
-                        <li><a href="#">java</a></li>
-                      </ul>
-                    </div>
-                </div>
-            </div>
+       @include('inc.question.right')
             
         </div>
       </div>
