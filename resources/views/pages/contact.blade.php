c2235c38e517c0c4ca1b5ab92438559600c15445
lirimhsn <lirimi1996@gmail.com>
2018-06-06 11:53:00 +0200

    map at contact form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -45,7 +45,12 @@
 
             <button class="btn btn-primary">Submit</button>
         </form>
+
         </div>
+        
+        <div class="map-responsive col-md-6" >
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d386950.6511603643!2d-73.70231446529533!3d40.738882125234106!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNueva+York!5e0!3m2!1ses-419!2sus!4v1445032011908" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
         </div>
         </div>
         
